373a8698e5bc91c0ca1b8b4653c15a0c6e13908d
Geckonda <Aquariusstd@yandex.ru>
2026-03-20 16:25:48 +0400

    init

---
+++ consumer-notification/src/main/java/com/kafkademo/consumer_notification/ConsumerNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.kafkademo.consumer_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerNotificationApplication.class, args);
+	}
+
+}
